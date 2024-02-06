a213212a6a5c2666eb52e2dd9d0635bd51f322d4
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-02-06 16:19:16 +0200

    [Task 3] - Made BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        self.updated_at = datetime.now()
+        
+    def to_dict(self):
+        new_dict = {}
+        new_dict['__class__'] = self.__class__.__name__
+        
+        for k, v in self.__dict__.items():
+            if k == "created_at" or k == "updated_at":
+                v = v.isoformat()
+            new_dict[k] = v
+
+        return new_dict
